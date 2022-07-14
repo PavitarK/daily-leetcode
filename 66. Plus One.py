6ddaffceb1cb86c8d6a9f01d8f755860d121344b
PavitarK <me@pavitar.ca>
2022-07-13 21:40:21 -0700

    problem 66

---
+++ 66. Plus One.py
@@ -0,0 +1,11 @@
+"""
+Leetcode Problem 66
+Plus One
+"""
+
+def plusOne(digits: list) -> list: 
+    
+    
+if __name__ == "__main__":
+    result = plusOne(digits=digits) 
+    print(f"Final Answer = {result}")
