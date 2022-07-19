b6544b102822d7bd88d94ba1d76ea51e7518c79b
PavitarK <me@pavitar.ca>
2022-07-19 15:50:54 -0700

    starting problem 49

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,16 @@
+"""
+Leetcode Problem 49
+Group Anagrams
+"""
+
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    
+    return
+    
+if __name__ == "__main__": 
+    strs = ["tea"]
+    result = groupAnagrams(strs=strs)
