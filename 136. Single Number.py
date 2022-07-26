cdbfa5b1dad5d9a656437b5a8209d69356c028d3
PavitarK <me@pavitar.ca>
2022-07-25 18:21:28 -0700

    136 done faster

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -2,7 +2,9 @@
 Leetcode Problem 136 
 Single Number
 """ 
-     
+
+# Brute Force, check every element and keep track of 
+# what has already appeared once
 def singleNumber(nums): 
     once = []
     twice = []
@@ -15,3 +17,22 @@ def singleNumber(nums):
             once.append(number)
             
     return once[0]
+
+# Use sorting (much faster)
+# sort array then just parse once and see which
+# number doesn't have a pair 
+# don't need to parse the whole array
+
+def singleNumber(nums): 
+    nums.sort()
+
+    for number in range(0,len(nums),2):
+        # if reached end of list it must be the last element
+        if number == len(nums)-1: 
+            return nums[number]
+        
+        if nums[number] != nums[number+1]:
+            single = nums[number]
+            break
+            
+    return single
