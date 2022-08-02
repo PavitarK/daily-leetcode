594e2e62a4c95b3467666464059bf0bbb21ee1f3
PavitarK <me@pavitar.ca>
2022-08-01 22:29:41 -0700

    Done Problem 190

---
+++ 190. Reverse Bits.py
@@ -0,0 +1,15 @@
+"""
+Leetcode Problem 190
+Reverse Bits
+"""
+
+def reverseBits(n): 
+    reversed = '{:032b}'.format(n)[::-1]
+    return int(reversed,2)
+
+
+
+
+n = 0b00000010100101000001111010011100
+n = 0b11111111111111111111111111111101 
+print(reverseBits(n))
