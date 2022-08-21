2c8c279aac659a7ff0491424ac5be02c1e115db6
PavitarK <me@pavitar.ca>
2022-08-21 14:04:22 -0700

    Climbing stairs recursive and dynamic solutions added

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -8,6 +8,8 @@ def climbStairs(n):
     """
     :type n: int
     :rtype: int
+    
+    Naive approach. This works but takes too much memory and is too slow. 
     """
     add = True
     twos = 1 
@@ -37,6 +39,37 @@ def climbStairs(n):
         twos += 1
         print(len(unique))       
         
-            
 
-print(f"There are {climbStairs(n=5)} ways to climb to the top")
+def climbStairsRecursive(n): 
+    """
+    In this approach we are using the knowledge that getting to stair x
+    in steps of 1 and 2 is equal to how ways we can get to x-1 and x-2.
+    
+    This works but is too slow. 
+    """
+    
+    if n == 1 or n == 0: 
+        return 1
+    
+    else: 
+        return climbStairsRecursive(n-1) + climbStairsRecursive(n-2)
+
+def climbStairsDynamicFast(n): 
+    """
+    Using the same recursive logic however can speed things up by removing 
+    redundant calculations. For example if we have calculated climbStairs(2)
+    we don't need to do it again and again. 
+    
+    Instead keep an array to look up values build bottom up.
+    [1,1,2,3,5,8....]
+    """
+    # ways 0 and 1 are always 1, build pattern from there
+    ways = [1,1]
+    
+    for i in range(2,n+1): 
+        ways.append(ways[i-1] + ways[i-2])
+        
+    return ways[n]    
+    
+
+print(f"There are {climbStairsDynamicFast(n=40)} ways to climb to the top")
