4ef219d234d176c6f54e29a093e09885e29e3a5a
PavitarK <me@pavitar.ca>
2023-05-01 17:51:36 -0700

    Completed problem 217

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,22 @@
+"""
+Leetcode Problem 217 
+Contains Duplicate
+Given an integer array nums, 
+return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+"""
+
+def containsDuplicate(nums: list[int]) -> bool:
+    length = len(nums)
+    nums.sort()
+    i = 0
+    while i < length-1:
+        print(i) 
+        if nums[i] == nums[i+1]:
+            return True
+        i = i + 1
+
+    return False
+
+
+print(containsDuplicate(nums=[1,1,1,2,3,1]))
