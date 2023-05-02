c4979c5eace169e424b8ca65218041357489380f
PavitarK <me@pavitar.ca>
2023-05-01 20:06:33 -0700

    Removed redundent join on 243

--- 243. Valid Anagram.py
+++ 243. Valid Anagram.py
@@ -6,8 +6,8 @@ Valid Anagram
 def isAnagram(s: str, t: str) -> bool:
     length = len(s)
     if length == len(t): 
-        s_sort = ''.join(sorted(s))
-        t_sort = ''.join(sorted(t))
+        s_sort = sorted(s)
+        t_sort = sorted(t)
         if s_sort == t_sort: 
             return True
     return False
