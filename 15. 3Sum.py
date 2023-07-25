3371c4438898a38f0fb7c5099044596b85bfaba9
PavitarK <me@pavitar.ca>
2023-07-25 16:23:07 -0700

    Done faster 3sum solution

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -3,31 +3,41 @@ Leetcode Problem 15
 3Sum
 """
 
+# Fast solution beats 65% of python submissions
+# Leverages checking for duplicate array values to 
+# avoid double checking and adding triplets that we have
+# already found. 
 
 def threeSum(nums: list[int]) -> list[list[int]]:
     length = len(nums)
     result = []
-    pairs = []
     
     nums.sort()
     
-    for s in range(length-2): 
+    for s in range(length-2):
+        #skip any duplicate s values since we have already covered it's triplets
+        if s > 0 and nums[s] == nums[s-1]: 
+            continue
         i = s+1 
         k = length-1
         
-        while i<k and i>s and k<length: 
+        while i<k: 
             sum = nums[s] + nums[i] + nums[k]
             if sum == 0:
                 r = [nums[s],nums[i],nums[k]]
-                if r not in result:  
-                    result.append(r)
+                result.append(r)
+                # change both pointers since it is impossible to have another unique
+                # triplet while mainting two pointer (s and i or k)
                 i = i + 1
+                k = k - 1
+                while i<k and nums[i] == nums[i - 1]: # skip any 2nd val duplicates
+                    i = i + 1
+                while k>i and nums[k] == nums[k + 1]: # skip any 3rd val duplicates
+                    k = k - 1
             elif sum > 0: 
                 k = k - 1
             else: 
                 i = i + 1
-        
-    print(result)
 
     return result 
 
