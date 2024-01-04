3836e3d33b28a97b53174968a2e16208220c9e52
PavitarK <me@pavitar.ca>
2024-01-03 17:00:04 -0800

    Done problem 125

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,19 @@
+def isPalindrome( s: str) -> bool:
+    # Replace all spaces and remove punctuation
+    # convert upper case to lowercase
+    # two pointers first and last
+    # while first <= last check equal
+    # first ++ 
+    # last -- 
+    s = s.replace(" ", "")
+    s = s.translate(str.maketrans('', '', string.punctuation))
+    s = s.lower()
+    
+    first = 0 
+    last = len(s)-1
+    while first <= last: 
+        if s[first] != s[last]: 
+            return False
+        first +=1 
+        last -=1
+    return True
